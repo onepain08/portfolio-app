c3fe7b9cf369b065b29a2769a1d37b84dce1b807
Onepain <onepaintdap@gmail.com>
2022-05-25 20:23:29 +0100

    navbar completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './header.css';
+import heroImage from '../../assets/hero-image.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className='header-content'>
+        <h1>Hello, my name is</h1>
+        <h1>Ariel Tolwinski</h1>
+        <h1>I am web developer</h1>
+      </div>
+      <div className='header-image'>
+        <img src={heroImage}></img>
+      </div>
+    </div>
   )
 }
 
