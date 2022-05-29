f32b20ab33f589c57f135b73fea9869f051b9f86
Onepain <onepaintdap@gmail.com>
2022-05-29 20:13:44 +0100

    navbar mobile

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,23 @@
 import React from "react";
 import './Nav.css';
+import toggleHamburger from 'react-icons'
 
 function Nav(){
+    
+    const [menuState, setMenuState] = React.useState(false)
+    
+    function toggleMenu(){
+        setMenuState((prevMenuState)=> !prevMenuState)
+    }
+
     return(
         <div className="nav">
             <div className="logo">
                 <h2>Ariel</h2>
                 <h2>Tolwinski</h2>
             </div>
-            <ul>
+                <button className="toggle-menu-btn" onClick={toggleMenu}>M</button>
+            <ul className={menuState ? 'menu-open' : 'menu-closed'}>
                 <a href="#home"><li>Home</li></a>
                 <a href="#about"><li>About</li></a>
                 <a href="#projects"><li>Projects</li></a>
